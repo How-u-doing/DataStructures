9563461b97a5dd58002bba665acbdd5b924c69c3
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-08-07 22:32:59 +0800

    Add files via upload

--- LinearLists/LinkedLists/SinglyLinkedList/List.h
+++ LinearLists/LinkedLists/SinglyLinkedList/List.h
@@ -8,7 +8,6 @@
 #include<string>
 #include<cstdlib>
 #include"LinearList.h"
-using namespace std;
 
 template<typename T>
 struct Node {
@@ -21,9 +20,9 @@ struct Node {
 template<typename T>
 class List: public LinearList<T> {
 public:	
-	List() { first = new Node<T>; }					// constructor
-	List(const T& x) { first = new Node<T>(x); }	// constructor
-	List(List<T>& L); 								// copy constructor							
+	List() { first = new Node<T>; }					// constructor with no infomation in data zone of head node
+	List(const T& Info_for_head_node) { first = new Node<T>(Info_for_head_node); }	// constructor, & store info in head node
+	List(List<T>& L); 								// copy constructor
 	List<T>& operator=(List<T>& L);					// assignment operator= overloading	
 	virtual ~List() { clear(); }					// virtual destructor		
 	virtual int size()const { return length(); }	// get the size of the list
@@ -36,9 +35,9 @@ public:
 	virtual void Union(List<T>& L2);				// union of two lists, and store the result in *this
 	virtual void Intersection(List<T>& L2);			// intersection of two lists, and store the result in *this
 	virtual void input();							// input data via the console window
-	virtual void Import(const string& filename);	// import corresponding data from a local host file
+	virtual void Import(const std::string& filename);// import corresponding data from a local host file
 	virtual void output()const;						// output data via the console window
-	virtual void Export(const string& filename)const;// export corresponding data into a local host file
+	virtual void Export(const std::string& filename)const;// export corresponding data into a local host file
 	virtual bool isEmpty()const { return first->next == nullptr ? true : false; }
 	virtual bool isFull()const { return false; }	// function derived from base class LinearList, no practical meaning in linked list
 	virtual void sort() {/* do a sort in a specific manner which hinges on the data type T. */ }
@@ -50,6 +49,7 @@ public:
 	Node<T>* getHead()const { return first; }		// get the pointer to head node
 	Node<T>* locate(int i)const;					// locate the i-th item	and return the pointer to this node	
 	Node<T>* find(const T& x)const;					// find specified item x and return the pointer to this node
+	T& operator[](int index);						// access list items via []
 private:
 	Node<T>* first;									// pointer to the first node of the list 	
 };
@@ -60,11 +60,11 @@ List<T>::List(List<T>& L) {
 	// copy constructor
 	Node<T>* srcptr = L.getHead();
 	Node<T>* desptr = first = new Node<T>;
-	if (first == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }
+	if (first == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 	while (srcptr->next != nullptr) {
 		// copy data from L & construct dynamic linked list
 		desptr->next = new Node<T>(srcptr->next->data);
-		if (desptr->next == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }
+		if (desptr->next == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 		// move pointers backward
 		srcptr = srcptr->next;
 		desptr = desptr->next;
@@ -76,17 +76,31 @@ List<T>& List<T>::operator=(List<T>& L) {
 	// assignment operator= overloading
 	Node<T>* srcptr = L.getHead();
 	Node<T>* desptr = first = new Node<T>;
-	if (first == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }
+	if (first == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 	while (srcptr->next != nullptr) {
 		// copy data from L & construct dynamic linked list
 		desptr->next = new Node<T>(srcptr->next->data);
-		if (desptr->next == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }		
+		if (desptr->next == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 		srcptr = srcptr->next;
 		desptr = desptr->next;
 	}
 	return *this;
 }
 
+template<typename T>
+T& List<T>::operator[](int index) {	
+	// access list items via [], 1<=index<=length(), equivalent to getVal(int i)	
+	if (index < 1) { std::cerr << "Invalid argument index, index must be no less than 1." << std::endl; exit(1); }
+	Node<T>* curr = first->next;
+	while (--index && curr != nullptr)
+		curr = curr->next;
+	if (curr == nullptr) {
+		std::cerr << "Index exceeded the list's length." << std::endl;
+		exit(1);
+	}
+	else
+		return curr->data;		
+}						
 
 template<typename T>
 void List<T>::clear() {
@@ -103,10 +117,10 @@ void List<T>::clear() {
 template<typename T>
 int List<T>::length()const {
 	// get the list's length 
-	Node<T>*p = first->next;
+	Node<T>* curr = first->next;
 	int count = 0;
-	while (p != nullptr) {
-		p = p->next;
+	while (curr != nullptr) {
+		curr = curr->next;
 		++count;
 	}
 	return count;
@@ -118,7 +132,7 @@ Node<T>* List<T>::locate(int i)const {
 	 * In particular,	 i=0,	  return first(i.e. the pointer to first node)
 	 *				  i>length(), return nullptr
 	**/
-	if (i < 0) { cerr << "Invalid argument i, i must be no less than 0." << endl; exit(1); }
+	if (i < 0) { std::cerr << "Invalid argument i, i must be no less than 0." << std::endl; exit(1); }
 	Node<T>* curr = first;
 	int k = 0;
 	while (curr != nullptr) {		
@@ -157,10 +171,10 @@ int List<T>::search(const T& x)const {
 template<typename T>
 T& List<T>::getVal(int i)const {
 	// return the value of i-th item, i>=1
-	if (i < 1) { cerr << "Invalid argument i, i must be no less than 1." << endl; exit(1); }
+	if (i < 1) { std::cerr << "Invalid argument i, i must be no less than 1." << std::endl; exit(1); }
 	Node<T>* curr = locate(i);
 	if (curr != nullptr) { return curr->data; }
-	else { cerr << "Parameter i exceeds the list's length." << endl; exit(1); }
+	else { std::cerr << "Parameter i exceeded the list's length." << std::endl; exit(1); }
 }
 
 template<typename T>
@@ -178,14 +192,14 @@ bool List<T>::getVal(int i, T& x)const {
 template<typename T>
 void List<T>::setVal(int i, const T& x) {
 	if (i < 1) {
-		cout << "Setting value failed due to invalid argument i. i must satisfy i>=1." << endl;
+		std::cout << "Setting value failed due to invalid argument i. i must satisfy i>=1." << std::endl;
 		return;
 	}
 	Node<T>* curr = locate(i);
 	if (curr != nullptr) 							// locate successfully
 		curr->data = x;
 	else
-		cout << "Setting value failed because i exceeds the list's length." << endl;
+		std::cout << "Setting value failed because i exceeds the list's length." << std::endl;
 }
 
 template<typename T>
@@ -193,10 +207,10 @@ bool List<T>::insert(int i, const T& x) {
 	/* Insert a new element x after the i-th node, i>=0
 	 * In particular, i=0, insert x after head node. 
 	**/ 
-	if (i < 0) { cout << "Invalid argument i, i must be no less than 0." << endl; return false; }
+	if (i < 0) { std::cout << "Invalid argument i, i must be no less than 0." << std::endl; return false; }
 	Node<T>* curr = locate(i);
 	Node<T>* newNode = new Node<T>(x);
-	if (newNode == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }
+	if (newNode == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 	newNode->next = curr->next;
 	curr->next = newNode;
 	return true;
@@ -210,7 +224,7 @@ bool List<T>::append(const T& x) {
 	while (curr->next != nullptr)
 		curr = curr->next;
 	Node<T>* newNode = new Node<T>(x);
-	if (newNode == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }
+	if (newNode == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 	newNode->next = curr->next;
 	curr->next = newNode;
 	return true;
@@ -219,7 +233,7 @@ bool List<T>::append(const T& x) {
 template<typename T>
 bool List<T>::remove(int i, T& x) {
 	// remove the i-th node & store the removed value
-	if (i < 1) { cout << "Invalid argument i, i must be no less than 1." << endl; return false; }
+	if (i < 1) { std::cout << "Invalid argument i, i must be no less than 1." << std::endl; return false; }
 	Node<T>* delpre = locate(i - 1), *del;
 	del = delpre->next;
 	delpre->next = del->next;
@@ -231,7 +245,7 @@ bool List<T>::remove(int i, T& x) {
 template<typename T>
 bool List<T>::remove(int i) {
 	// remove the i-th node without storing it
-	if (i < 1) { cout << "Invalid argument i, i must be no less than 1." << endl; return false; }
+	if (i < 1) { std::cout << "Invalid argument i, i must be no less than 1." << std::endl; return false; }
 	Node<T>* delpre = locate(i - 1), *del;
 	del = delpre->next;
 	delpre->next = del->next;
@@ -259,7 +273,7 @@ void List<T>::Intersection(List<T>& L2) {
 	T x;
 	while (curr != nullptr) {
 		x = curr->data;
-		if (!L2.search(x)) {							// not found x in list L2			
+		if (!L2.search(x)) {						// not found x in list L2			
 			// erase x in this list
 			prev->next = curr->next;
 			Node<T>* del = curr;
@@ -276,14 +290,14 @@ void List<T>::Intersection(List<T>& L2) {
 template<typename T>
 void List<T>::input() {
 	if (first->next != nullptr) {					// this list has at least one node
-		cout << "Warning, the list is not null. Input new data will cover the original data\n";
-		cout << "Are you sure to go on?('y' or 'n')\n";
+		std::cout << "Warning, the list is not null. Input new data will cover the original data\n";
+		std::cout << "Are you sure to go on?('y' or 'n')\n";
 		char c;
 		// clear stdin buffer to avoid cin extracting '\n' for c
-		cin.clear();
-		cin.sync();
+		std::cin.clear();
+		std::cin.sync();
 
-		cin >> c;
+		std::cin >> c;
 		if (c != 'y' && c != 'Y') 					// cancel
 			return;	
 		// else execute following instructions
@@ -291,34 +305,34 @@ void List<T>::input() {
 	clear();
 	Node<T>* curr = first;
 	T tmp;
-	cout << "Please input data.  (Attention: to end up with Ctrl+Z)" << endl;
-	while (cin >> tmp) {
+	std::cout << "Please input data.  (Attention: to end up with Ctrl+Z)" << std::endl;
+	while (std::cin >> tmp) {
 		curr->next = new Node<T>(tmp);
-		if (curr->next == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }
+		if (curr->next == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 		curr = curr->next;
 	}	
 }
 
 template<typename T>
-void List<T>::Import(const string& filename) {
+void List<T>::Import(const std::string& filename) {
 	if (first->next != nullptr) {					// this list has at least one node
-		cout << "Warning, the list is not null. Input new data will cover the original data\n";
-		cout << "Are you sure to go on?('y' or 'n')\n";
+		std::cout << "Warning, the list is not null. Input new data will cover the original data\n";
+		std::cout << "Are you sure to go on?('y' or 'n')\n";
 		char c;
 		// clear stdin buffer to avoid cin extracting '\n' for c
-		cin.clear();
-		cin.sync();
+		std::cin.clear();
+		std::cin.sync();
 
-		cin >> c;
+		std::cin >> c;
 		if (c != 'y' && c != 'Y')					// cancel
 			return;
 		// else execute following instructions
 	}
 	clear();
-	ifstream is(filename, ios::in | ios::binary);
+	std::ifstream is(filename, std::ios::in | std::ios::binary);
 	if (!is) {
-		cerr << "Open file \"" << filename << "\" error! Can't find this file.\n";
-		cout<<"Please check the validity of its directory or filename." << endl;
+		std::cerr << "Open file \"" << filename << "\" error! Can't find this file.\n";
+		std::cout<<"Please check the validity of its directory or filename." << std::endl;
 		exit(1);
 	}
 
@@ -326,7 +340,7 @@ void List<T>::Import(const string& filename) {
 	Node<T>* curr = first;
 	while (is.read((char*) &tmp, sizeof(T))) {
 		curr->next = new Node<T>(tmp);
-		if (curr->next == nullptr) { cerr << "Memory allocation error!" << endl; exit(1); }
+		if (curr->next == nullptr) { std::cerr << "Memory allocation error!" << std::endl; exit(1); }
 		curr = curr->next;
 	}	
 	is.close();
@@ -337,17 +351,16 @@ void List<T>::output()const {
 	Node<T>* curr = first->next;
 	int i=1;
 	while (curr != nullptr) {
-		cout << "#" << i++ << ": " << curr->data << endl;
+		std::cout << "#" << i++ << ": " << curr->data << std::endl;
 		curr = curr->next;
 	}
 }
 
-
 template<typename T>
-void List<T>::Export(const string& filename)const {
-	ofstream os(filename, ios::out | ios::binary | ios::_Noreplace);
+void List<T>::Export(const std::string& filename)const {
+	std::ofstream os(filename, std::ios::out | std::ios::binary | std::ios::_Noreplace);
 	if (!os) {
-		cerr << "Open file error! File \"" << filename << "\" has already existed." << endl;
+		std::cerr << "Open file error! File \"" << filename << "\" has already existed." << std::endl;
 		exit(1);
 	}
 	
--- LinearLists/LinkedLists/SinglyLinkedList/List_test.cpp
+++ LinearLists/LinkedLists/SinglyLinkedList/List_test.cpp
@@ -1,48 +1,52 @@
 #include<iostream>
 #include<string>
-#include<iostream>
 #include "List.h"
 using namespace std;
+
 int main()
 {
-	List<string> ls,ls2,ls3,ls4,ls5,ls6;
-	ls.append("Ania");
-	ls.append("Rachel");
-	ls.insert(1, "Joey");
-	List<string> ls1 = ls;
-	ls1.insert(2, "Honey");
-	ls1.append("Marvlous");
-	ls1.remove(2);
-	cout << "LS: " << endl;
+	List<string>  ls{ "*** Friends Characters ***" }, ls2, ls3, ls4, ls5;
+	// input following content via console window
+	// Chandler Ross Joey Monica Rachel Phoebe
+	// Ctrl+Z
+
+	// input() test
+	ls.input();
+	cout << "LS: " << endl << ls.getHead()->data << endl;
 	ls.output();
-	cout << "LS1: " << endl;
+
+	// getVal() & operator[] test 
+	cout << ls.getVal(3) << " once had a huge crush on " << ls[5] << endl;
+
+	// copy constructor test
+	List<string>  ls1{ ls };				// i.e.  List<string>  ls2(ls);
+
+	// remove(), insert() & append() test
+	ls1.remove(6);
+	ls1.remove(1);
+	ls1.insert(0,"Iron Man");
+	ls1.append("Captain America");
+	cout << "\nLS1: " << endl;
 	ls1.output();
+	
+	// operator= test 
 	ls2 = ls;
 	ls3 = ls1;
+
+	// Union(), Intersection() test
 	ls2.Union(ls1);
-	cout << "LS2, union of ls & ls1: " << endl;
+	cout << "\nLS2, union of LS & LS1: " << endl;
 	ls2.output();
-	cout << "LS3, intersection of ls & ls1: " << endl;
 	ls3.Intersection(ls);
+	cout << "\nLS3, intersection of LS & LS1: " << endl;
 	ls3.output();
-	ls4 = ls;
-	cout << "LS4(=LS): " << endl;
+
+	// Export(), Import() test
+	ls.Export("Friends.dat");
+	ls4.Import("Friends.dat");
+	cout << "\nLS4, imported from LS: " << endl; 
 	ls4.output();
-	ls4.Export("fantastic.dat");
-	ls5.Import("fantastic.dat");
-	cout << "LS5(imported from fantastic.dat): " << endl;
-	ls5.output();
-	cout << "ls5-> member 2:" << endl << ls5.getVal(2) << endl;
-	ls5.insert(0, "Chandler");
-	ls5.append("Monica");
-	ls5.output();
-	cout << "ls5-> member 1:" << endl << ls5.getVal(1) << endl;
-	ls6.input();
-	cout << "LS6, input via console window: " << endl;
-	ls6.output();
-	cout << "ls6-> member 2:" << endl << ls6.getVal(2) << endl;
-	ls6.append("Stunning view");
-	ls6.input();
-	ls6.output();
+
+	cout << "\nEnd of test " << endl;
 	return 0;
 }
