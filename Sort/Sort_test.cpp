2ee508ef9adca65bdb7ba034a4bb2d8d7295abb8
Mark Taylor <2942224119@qq.com>
2020-04-25 23:58:43 +0800

    Add binary insertion sort

--- Sort/Sort_test.cpp
+++ Sort/Sort_test.cpp
@@ -1,34 +1,68 @@
 #include "mySort.h"
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-struct stu {
+class stu {
+private:
 	//int Number:3;	   // using only 3 bits for Number
 	int Number;
 	float Math;
 	float English;
 	float Computer;
-	float total_score = sum();
-	float sum()const { return Math + English + Computer; }
+	float total_score;
 
+public:
 	stu(int num = 0, float math = .0f, float english = .0f, float computer = .0f)
-		:Number(num), Math(math), English(english), Computer(computer) {}
+		:Number(num), Math(math), English(english), Computer(computer), total_score(calc_sum()) {}
 
-	friend ostream& operator<< (ostream & os, const stu & s) {
-		os << s.Number << "\t" << s.Math << "\t" << s.English << "\t" << s.Computer << "\t" << s.total_score << endl;
-		return os;
+	stu(const stu& s) :Number(s.Number), Math(s.Math), English(s.English),
+		Computer(s.Computer), total_score(s.total_score) {}
+
+	stu& operator=(const stu& s) {
+		Number = s.Number;
+		Math = s.Math;
+		English = s.English;
+		Computer = s.Computer;
+		total_score = s.total_score;
+		return *this;
 	}
 
 	static void printHeader() {
 		cout << " Number\t      Math   English  Computer  Sum\n";
 	}
+
+	friend ostream& operator<< (ostream & os, const stu & s) {
+		os << s.Number << "\t" << s.Math << "\t" << s.English << "\t" << s.Computer << "\t" << s.total_score;
+		return os;
+	}
+
+	friend bool operator<(const stu& a, const stu& b) {
+		return a.get_sum() < b.get_sum();
+	}
+
+	float calc_sum()const { return Math + English + Computer; }
+	float get_sum()const { return total_score; }
 };
 
-bool sum_comp(const stu& s1, const stu& s2) {
-	return s1.total_score > s2.total_score;
+bool sum_comp(const stu& a, const stu& b) {
+	return a.get_sum() > b.get_sum();
+}
+
+constexpr int square(int n) {
+	return n * n;
+}
+
+void kill_extra_char()
+{
+#if defined _WIN32 || defined _WIN64
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+#else  // on Linux/Unix
+	cin.ignore(1024, '\n');
+#endif // defined _WIN32 || defined _WIN64
 }
 
 int main()
@@ -39,7 +73,7 @@ int main()
 	vs.push_back(stu(10170402, 61.5, 70, 61));
 	vs.push_back(stu(10170403, 92, 89, 98));
 	vs.push_back(stu(10170404, 70, 94, 90));
-	vs.push_back(stu(10170405, 83, 76, 91));
+	vs.push_back(stu(10170405, 83, 76, 93));
 	vs.push_back(stu(10170406, 78, 79, 81.5));
 	vs.push_back(stu(10170407, 92, 97, 100));
 	vs.push_back(stu(10170408, 65, 90.5, 79));
@@ -48,27 +82,195 @@ int main()
 	cout << "Original list\n";
 	stu::printHeader();
 	for (auto& item : vs) {
-		cout << item;
+		cout << item << '\n';
 	}
 
-	// using std sorting algorithm
-	/*std::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
-		return s1.total_score > s2.total_score;
-		});*/
+	// test build-in array	
+	int a[square(3)] = { 4,9,1,3,5,7,2,8,6 };
+	cout << "\nOriginal int array\n";
+	for (int i = 0; i < 9; ++i) {
+		i < 8 ? cout << a[i] << ", " : cout << a[i] << '\n';
+	}
 
-	// using my sorting algorithm
-	mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
-		return s1.total_score > s2.total_score;
-		}, mySortingAlgo::Mode::BubbleSort);
 
-	// try default sort (Quicksort)
-	//mySortingAlgo::sort(vs.begin(), vs.end(), sum_comp);
+	cout << "\nPlease choose sorting algorithm\n";
+	cout << "** Insertion Sort------------------------- 0 \n";
+	cout << "** Selection Sort ------------------------ 1 \n";
+	cout << "** Merge Sort ---------------------------- 2 \n";
+	cout << "** Heapsort ------------------------------ 3 \n";
+	cout << "** Quicksort ----------------------------- 4 \n";
+	cout << "** Shellsort ----------------------------- 5 \n";
+	cout << "** Bubble Sort --------------------------- 6 \n";
+	cout << "** Cumb Sort ----------------------------- 7 \n";
+	cout << "** Counting Sort ------------------------- 8 \n";
+	cout << "** Bucket Sort --------------------------- 9 \n";
+	cout << "** Radix Sort --------------------------- 10 \n";
+	cout << "Enter your option (0-10) here: ";
 
-	cout << "\n\nAfter sorting by total score\n";
+	char isValid = 'y';
+	int mode{};
+	while (isValid == 'y')
+	{		
+		cin >> mode;
+		while (cin.fail()) {// possibly a formatting error, e.g. inputed a non-digit first
+			cin.clear();
+			kill_extra_char();
+			cout << "Invalid char detected! Please input your option (0-10) again: ";
+			cin >> mode;
+		}
+
+		switch (mode)
+		{
+		case 0: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::InsertionSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::InsertionSort);
+
+			isValid = 'n'; break;
+
+		case 1: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::SelectionSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::SelectionSort);
+
+			isValid = 'n'; break;
+
+		case 2: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::MergeSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::MergeSort);
+
+			isValid = 'n'; break;
+
+		case 3: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::Heapsort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::Heapsort);
+
+			isValid = 'n'; break;
+
+		case 4: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::Quicksort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::Quicksort);
+
+			isValid = 'n'; break;
+
+		case 5: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::Shellsort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::Shellsort);
+
+			isValid = 'n'; break;
+
+		case 6: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::BubbleSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::BubbleSort);
+
+			isValid = 'n'; break;
+
+		case 7: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::CombSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::CombSort);
+
+			isValid = 'n'; break;
+
+		case 8: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::CountingSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::CountingSort);
+
+			isValid = 'n'; break;
+
+		case 9: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::BucketSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::BucketSort);
+
+			isValid = 'n'; break;
+
+		case 10: mySortingAlgo::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+			return s1.get_sum() > s2.get_sum();
+			}, mySortingAlgo::Mode::RadixSort);
+
+			mySortingAlgo::sort(a, a + 9, [](const int a, const int b) {
+				return a < b;
+				}, mySortingAlgo::Mode::RadixSort);
+
+			isValid = 'n'; break;
+
+		default:
+			cout << "Invalid option! Please input your option (0-10) again: ";
+			kill_extra_char();
+			break;
+		}
+	}
+
+	string method[]{ "Insertion Sort", "Selection Sort", "Merge Sort", "Heapsort", "Quicksort", "Shellsort", 
+					 "Bubble Sort", "Comb Sort", "Counting Sort", "Bucket Sort", "Radix Sort"};
+
+	string print_msg = "The student list and build-in int array sorted by \"" + method[mode] + "\" are as follows\n";
+	cout << print_msg;
+
+	cout << "\n**Student** --> After sorting by total score in descending order\n";
 	stu::printHeader();
 	for (auto& item : vs) {
-		cout << item;
+		cout << item << '\n';
 	}
 
+	cout << "\n**Build-in array** --> After sorting in ascending order\n";
+	for (int i = 0; i < 9; ++i) {
+		i < 8 ? cout << a[i] << ", " : cout << a[i] << '\n';
+	}
+
+	cout << "\nPress any key to leave...\n";
+	char wait;
+	kill_extra_char();
+	cin >> wait;	
 	return 0;
+
+	// Appendix: using std sorting algorithm (Quicksort)
+	/*std::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+		return s1.get_sum() > s2.get_sum();
+		});*/
+	// bad, invalid comparator, return value can be only true(!= 0) or false(== 0)
+	// see also <https://en.cppreference.com/w/cpp/algorithm/sort>
+	/*std::sort(vs.begin(), vs.end(), [](const stu& s1, const stu& s2) {
+		if (s1.get_sum() > s2.get_sum()) return 1;
+		else if (s1.get_sum() < s2.get_sum()) return -1;
+		else return 0;
+		});*/
 }
+
--- Sort/mySort.h
+++ Sort/mySort.h
@@ -17,7 +17,7 @@ enum class Mode
 	// bubble sort and variants
 	BubbleSort, CombSort,
 
-	// Distribution sort
+	// distribution sort
 	CountingSort, BucketSort, RadixSort
 
 };
@@ -25,39 +25,59 @@ enum class Mode
 template<typename RandomIt, typename Compare>
 class sortingMethods {
 public:
+	static void InsertionSort(RandomIt first, RandomIt last, Compare comp);
+	static void SelectionSort(RandomIt first, RandomIt last, Compare comp);
+	static void MergeSort(RandomIt first, RandomIt last, Compare comp);
+	static void Heapsort(RandomIt first, RandomIt last, Compare comp);
+	static void Quicksort(RandomIt first, RandomIt last, Compare comp);
+	static void Shellsort(RandomIt first, RandomIt last, Compare comp);
 	static void BubbleSort(RandomIt first, RandomIt last, Compare comp);
-	//... to be added others
+	static void CombSort(RandomIt first, RandomIt last, Compare comp);
+	static void CountingSort(RandomIt first, RandomIt last, Compare comp);
+	static void BucketSort(RandomIt first, RandomIt last, Compare comp);
+	static void RadixSort(RandomIt first, RandomIt last, Compare comp);
 
-	static inline void swap(RandomIt it_1, RandomIt it_2);
+	static inline void swap_content(RandomIt it_1, RandomIt it_2);
 };
 
+/* Sort an array (or a list) ranging [first, last) */ 
 template<typename RandomIt, typename Compare>
 void sort(RandomIt first, RandomIt last, Compare comp, Mode mode = Mode::Quicksort)
 {
 	switch (mode)
 	{
 	case Mode::InsertionSort:
+		sortingMethods<RandomIt, Compare>::InsertionSort(first, last, comp);
 		break;
 	case Mode::SelectionSort:
+		sortingMethods<RandomIt, Compare>::SelectionSort(first, last, comp);
 		break;
 	case Mode::MergeSort:
+		sortingMethods<RandomIt, Compare>::MergeSort(first, last, comp);
 		break;
 	case Mode::Heapsort:
+		sortingMethods<RandomIt, Compare>::Heapsort(first, last, comp);
 		break;
 	case Mode::Quicksort:
+		sortingMethods<RandomIt, Compare>::Quicksort(first, last, comp);
 		break;
 	case Mode::Shellsort:
+		sortingMethods<RandomIt, Compare>::Shellsort(first, last, comp);
 		break;
 	case Mode::BubbleSort:
 		sortingMethods<RandomIt, Compare>::BubbleSort(first, last, comp);
 		break;
 	case Mode::CombSort:
+		sortingMethods<RandomIt, Compare>::CombSort(first, last, comp);
 		break;
 	case Mode::CountingSort:
+		sortingMethods<RandomIt, Compare>::CountingSort(first, last, comp);
 		break;
 	case Mode::BucketSort:
+		sortingMethods<RandomIt, Compare>::BucketSort(first, last, comp);
 		break;
 	case Mode::RadixSort:
+		sortingMethods<RandomIt, Compare>::RadixSort(first, last, comp);
 		break;
 	default:
 		break;
@@ -66,21 +86,139 @@ void sort(RandomIt first, RandomIt last, Compare comp, Mode mode = Mode::Quickso
 
 // This one has a drawback that cannot call lambda function
 //template<class RandomIt, class U>
-//void sort(RandomIt first, RandomIt last, bool (*comp)(const U& a, const U& b))
+//void sort(RandomIt first, RandomIt last, bool (*comp)(const U& a, const U& b), Mode mode = Mode::Quicksort)
+//{
+//	// implementation...
+//}
+// Or like this using only one template argument via decltype
+//template<class RandomIt>
+//void sort(RandomIt first, RandomIt last, bool (*comp)(const decltype(*first)& a, const decltype(*first)& b), Mode mode = Mode::Quicksort)
 //{
 //	// implementation...
 //}
 
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::InsertionSort(RandomIt first, RandomIt last, Compare comp)
+{
+	// see also <https://en.wikipedia.org/wiki/Insertion_sort>
+	// direct insertion sort
+	/*int len = last - first;
+	for (int i = 1; i < len; ++i) {
+		for (int j = i; j > 0 && comp(*(first + j), *(first + j - 1)); --j) {
+			swap_content(first + j, first + j - 1);
+		}
+	}*/
+
+	//=========================================================================
+	//=========================================================================
+
+	// When we have massive data items, we can save some comparison time by
+	// using binary search, see <https://en.wikipedia.org/wiki/Binary_search_algorithm>.
+	// determine the sorting order: ascending or descending
+	// of course, overloading operator< prior to comparison is needed
+	int order{ 0 };
+	if (last - first > 1) {
+		if (*first < *(first + 1)) {
+			if (comp(*first, *(first + 1)))
+				order = 1;		// ascending
+			else
+				order = -1;		// descending
+		}
+		else {// *first >= *(first + 1)
+			if (comp(*first, *(first + 1)))
+				order = -1;		// descending
+			else
+				order = 1;		// ascending
+		}
+	}
+	else
+		return;
+
+	decltype(first) L{ }, R{ }, M{ };
+	// we can decrease the times of comparison of sorting
+	// order to 1 with the cost of somewhat repetitive code
+	if (order == 1) {// in ascending order
+		for (auto it = first + 1; it != last; ++it) {
+			L = first; R = it - 1;
+			while (L <= R) {
+				M = L + (R - L) / 2;	// how about M=(L+R)/2 ?
+				if (*it < *M) {
+					if (M == first)
+						break;
+					else
+						R = M - 1;
+				}
+				else
+					L = M + 1;
+			}
+			// insert *it right at position L, move by block
+			auto tmp = *it;
+			for (auto iter = it; iter != L; --iter)
+				*iter = *(iter - 1);
+			*L = tmp;
+		}
+	}
+	else {// in descending order
+		for (auto it = first + 1; it != last; ++it) {
+			L = first; R = it - 1;
+			while (L <= R) {
+				M = L + (R - L) / 2;
+				if (*M < *it) {
+					if (M == first)
+						break;
+					else
+						R = M - 1;
+				}
+				else
+					L = M + 1;
+			}
+			// insert *it right at position L, move by block
+			auto tmp = *it;
+			for (auto iter = it; iter != L; --iter)
+				*iter = *(iter - 1);
+			*L = tmp;
+		}
+	}
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::SelectionSort(RandomIt first, RandomIt last, Compare comp)
+{
+
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::MergeSort(RandomIt first, RandomIt last, Compare comp)
+{
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::Heapsort(RandomIt first, RandomIt last, Compare comp)
+{
+
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::Quicksort(RandomIt first, RandomIt last, Compare comp)
+{
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::Shellsort(RandomIt first, RandomIt last, Compare comp)
+{
+}
 
 template<typename RandomIt, typename Compare>
 void sortingMethods<RandomIt, Compare>::BubbleSort(RandomIt first, RandomIt last, Compare comp)
 {
-	int n = last - first;
-	while (n >= 1) {
-		int lastSwappedIndex = 0;
-		for (int i = 0; i < n - 1; ++i) {
-			if (comp(*(first + i + 1), *(first + i))) {
-				swap(first + i, first + i + 1);
+	// see also <https://en.wikipedia.org/wiki/Bubble_sort#Optimizing_bubble_sort>
+	int n = last - first;	// unsorted length
+	int lastSwappedIndex = 0;
+	while (n > 1) {
+		lastSwappedIndex = 0;
+		for (int i = 1; i < n; ++i) {
+			if (comp(*(first + i), *(first + i - 1))) {
+				swap_content(first + i, first + i - 1);
 				lastSwappedIndex = i;
 			}
 		}
@@ -89,7 +227,27 @@ void sortingMethods<RandomIt, Compare>::BubbleSort(RandomIt first, RandomIt last
 }
 
 template<typename RandomIt, typename Compare>
-inline void sortingMethods<RandomIt, Compare>::swap(RandomIt it_1, RandomIt it_2)
+void sortingMethods<RandomIt, Compare>::CombSort(RandomIt first, RandomIt last, Compare comp)
+{
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::CountingSort(RandomIt first, RandomIt last, Compare comp)
+{
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::BucketSort(RandomIt first, RandomIt last, Compare comp)
+{
+}
+
+template<typename RandomIt, typename Compare>
+void sortingMethods<RandomIt, Compare>::RadixSort(RandomIt first, RandomIt last, Compare comp)
+{
+}
+
+template<typename RandomIt, typename Compare>
+inline void sortingMethods<RandomIt, Compare>::swap_content(RandomIt it_1, RandomIt it_2)
 {
 	auto tmp = *it_1;
 	*it_1 = *it_2;
