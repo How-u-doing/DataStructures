9932ae4ad5ff4fc8bcb25128c1d2d887804bafb1
Mark Taylor <2942224119@qq.com>
2020-07-30 01:40:01 +0800

    Add paths, connected component

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,8 +1,11 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <queue>
+#include <utility> // std::swap
 #include <fstream>
+#include <iostream>
 #include <stdexcept>
 
 // undirected graph
@@ -13,19 +16,37 @@ struct GraphReadException : std::runtime_error {
 	GraphReadException(const std::string& mesg) : std::runtime_error(mesg) {}
 };
 
+struct GraphEdgeException : std::runtime_error {
+	GraphEdgeException(const char* mesg) : std::runtime_error(mesg) {}
+	GraphEdgeException(const std::string& mesg) : std::runtime_error(mesg) {}
+};
+
+struct GraphInternalException : std::runtime_error {
+	GraphInternalException(const char* mesg) : std::runtime_error(mesg) {}
+	GraphInternalException(const std::string& mesg) : std::runtime_error(mesg) {}
+};
+
+struct GraphIterOutOfRange : std::out_of_range {
+	GraphIterOutOfRange(const char* mesg) : std::out_of_range(mesg) {}
+	GraphIterOutOfRange(const std::string& mesg) : std::out_of_range(mesg) {}
+};
+
 struct Edge {
-	Edge(size_t dist, double cost = 0.0) :_dist(dist), _cost(cost) {}
-	size_t _dist;	// pos of the other vertex in adjacent list
+	Edge(size_t dest, double cost = 0.0) :_dest(dest), _cost(cost) {}
+	size_t _dest;	// pos of the other vertex in adjacent list
 	double _cost;	// weight
 };
 
 template<typename T = std::string>
 class Graph {
 public:
+	using iterator = typename std::unordered_map<T, size_t>::iterator;
+	using const_iterator = typename std::unordered_map<T, size_t>::const_iterator;
+
 	Graph() {}
 
 	Graph(const std::string& filename, bool weighted = true) {
-		std::ifstream ifs(filename, std::ios_base::in);		
+		std::ifstream ifs(filename, std::ios_base::in);
 		if (!ifs.is_open())
 			throw std::runtime_error("Error opening file \"" + filename + "\"");
 
@@ -36,7 +57,12 @@ public:
 			if (weighted) {
 				if (!(ifs >> cost)) throw GraphReadException("Error reading edge weight from \"" + filename + "\"");
 			}
-			add_edge(v, w, cost);
+			add_vertex(v);	add_vertex(w);	// do nothing if exist
+			auto p1{ find_vertex(v) }, p2{ find_vertex(w) };
+			if (!has_edge_unchecked(p1, p2))
+				add_edge_by_index_unchecked(p1->second, p2->second, cost);
+			else // or we may just need to overwrite it & print a message
+				throw GraphReadException("Found edge duplicated while reading \"" + filename + "\"");
 		}
 		// file will be closed when leaving scope (regardless of exception),
 		// guaranteed by RAII (Resource Acquisition Is Initialization)
@@ -50,63 +76,278 @@ public:
 
 	~Graph() {}
 
-	void clear() { _adj.clear(); _map.clear(); _E = 0; }
+	void clear() { _adj.clear(); _map.clear(); _keys.clear();  _E = 0; }
 
 	void output_graph(std::ostream& os, bool weighted = true) {
-		// invert indices to keys
-		T* keys = new T[_map.size()]{};
-		for (const auto& pair: _map)
-			keys[pair.second] = pair.first;
-
 		for (size_t i = 0; i < vertex_size(); ++i) {
-			os << keys[i] << '\n';
+			os << _keys[i] << '\n';
 			for (const auto& x : _adj[i]) {
-				os << "   " << keys[x._dist];
+				os << "   " << _keys[x._dest];
 				if (weighted)
 					os << "\t" << x._cost;
 				os << '\n';
 			}
 		}
-		delete[] keys;
 	}
-
+private:
+	size_t index(iterator i) {
+		if (i == end()) throw GraphIterOutOfRange("Cannot get vertex to end()");
+		return i->second;
+	}
+	size_t index(const T& v) {
+		return _map.at(v);
+	}
+public:
+	T vertex(iterator i) {
+		if(i==end()) throw GraphIterOutOfRange("Cannot get vertex to end()");
+		return i->first;
+	}
+	
 	size_t vertex_size() const noexcept { return _adj.size(); }
 	size_t edge_size() const noexcept { return _E; }
 
-	// if vertex v, w are not present, add them; then add edge
-	void add_edge(const T& v, const T& w, double cost = 0.0) {
-		add_vertex(v);	add_vertex(w);	// do nothing if exist
-		add_edge_by_indices(_map[v], _map[w], cost);
-		++_E;
+	iterator begin() noexcept { return _map.begin(); };
+	iterator end() noexcept { return _map.end(); };
+	const_iterator begin() const noexcept { return _map.begin(); };
+	const_iterator end() const noexcept { return _map.end(); };
+
+	iterator find_vertex(const T& v) { return _map.find(v); }
+	const_iterator find_vertex(const T& v) const { return _map.find(v); }
+
+	// since this is a undirected graph, from/to are equivalent
+	bool has_edge(const_iterator from, const_iterator to) const {
+		if (from == end() || to == end()) return false;
+		return has_edge_unchecked(from, to);
 	}
 
-	// add vertex if it dosen't exist
+	void add_edge(iterator from, iterator to, double cost = 0.0) {
+		if (from == end() || to == end())
+			throw GraphIterOutOfRange("Cannot add edge from/to vertex to end()");
+		if (has_edge_unchecked(from, to))
+			throw GraphEdgeException("Cannot add edge since it already existed");
+		else
+			add_edge_by_index_unchecked(from->second, to->second, cost);
+	}
+
+	void modify_weight(iterator v, iterator w, double cost) {
+		if (v == end() || w == end())
+			throw GraphIterOutOfRange("Cannot modify edge from/to vertex to end()");
+		auto p{ find_dest(v->second, w->second) }, q{ find_dest(w->second, v->second) };
+		if (p != _adj[w->second].end() && q != _adj[v->second].end()) {
+			p->_cost = cost; q->_cost = cost;
+		}
+		else if (p == _adj[w->second].end() && q == _adj[v->second].end())
+			throw GraphEdgeException("Cannot modify edge since it doesn't exist");
+		else // should never happen
+			throw GraphInternalException("Graph internal structure damaged");
+	}
+	
+	void remove_edge(iterator v, iterator w) {
+		if (v == end() || w == end())
+			throw GraphIterOutOfRange("Cannot remove edge from/to vertex to end()");
+		auto p{ find_dest(v->second, w->second) }, q{ find_dest(w->second, v->second) };
+		if (p != _adj[w->second].end() && q != _adj[v->second].end()) {
+			_adj[w->second].erase(p); _adj[v->second].erase(q);
+		}
+		else if (p == _adj[w->second].end() && q == _adj[v->second].end())
+			throw GraphEdgeException("Cannot remove edge since it doesn't exist");
+		else
+			throw GraphInternalException("Graph internal structure damaged");
+	}
+	
+	// add vertex, if it dosen't exist
 	void add_vertex(const T& v) {
-		if (!has_vertex(v))
+		if (find_vertex(v) == end())
 			add_vertex_unchecked(v);
 	}
 
-	bool has_vertex(const T& v) {
-		return _map.find(v) != _map.end();
+	// remove vertex, if any
+	void remove_vertex(iterator v) {
+		if (v == end()) throw GraphIterOutOfRange("Cannot remove vertex to end()");
+		size_t vi{ v->second };	// index of v
+		// remove all edges point to v
+		for (const auto& w : _adj[vi])
+			remove_edge_v_in_w(vi, w._dest);
+
+		size_t last{ vertex_size() - 1 };
+		// swap vertex v with last vertex & change relative edges
+		if (vi != last) {
+			_adj[vi].swap(_adj[last]);
+			// modify all edges that have a destination to last with vi
+			for (const auto& x : _adj[vi]) {
+				auto it = find_dest(last, x._dest);
+				if (it != _adj[x._dest].end())	it->_dest = vi;
+				else throw GraphEdgeException("Cannot add edge since it already existed");
+			}
+			// swap vertices in associated containers
+			std::swap(_map.find(_keys[last])->second, v->second);
+			std::swap(_keys[last], _keys[vi]);
+		}
+		// remove vertex with index 'last'
+		_adj.pop_back();
+		_map.erase(v);
+		_keys.pop_back();			
 	}
 
+	bool has_path(iterator from, iterator to) {
+		std::vector<bool> marked(vertex_size(), false);
+		DFS_hp(from, marked);
+		return marked[index(to)];
+	}
+
+	void path(iterator from, iterator to, std::vector<T>& path) {
+		if (from == end() || to == end())
+			throw GraphIterOutOfRange("DFS_path iterator out of range");
+		std::vector<bool> marked(vertex_size(), false);
+		std::vector<size_t> edge_to(vertex_size(), 0);
+		size_t s = from->second, v = to->second;
+		DFS_path(s, marked, edge_to);
+		if (!marked[index(to)]) return;	// no path
+		// add paths in reverse order
+		for (size_t x = v; x != s; x = edge_to[x])
+			path.push_back(_keys[x]);
+		path.push_back(_keys[s]); // add source to 'beginning' (after reverse)
+		// reverse back
+		for (auto p1 = path.begin(), p2 = path.end() - 1; p1 < p2;) {
+			std::swap(*p1++, *p2--);
+		}
+		// note that we can also use an auxiliary integer vector for
+		// storing the vertex indices, then copy back in reverse order
+		/*for (auto p = ipath.rbegin(); p != ipath.rend(); ++p)
+			path.push_back(_keys[*p]);*/
+		// where ipath is a vector<size_t> replaced path before
+	}
+
+	void connected_component() {
+		// A good way to reduce the times of tell if visited is to get a vertex
+		// from each connected component of the graph, the overhead of which is,
+		// in general, regardless of time complexity or algorithm complexity, 
+		// greater than each vertex traversal. So let's just do it :)
+		std::vector<bool> marked(vertex_size(), false);
+		size_t count{ 0 };
+		std::unordered_map<T, size_t> ump;
+		for (auto it = _map.begin(); it != _map.end(); ++it) {
+			if (!marked[it->second]) {
+				DFS_cc(it, marked, count, ump);
+				++count;
+			}
+		}
+		auto components = new std::vector<T>[count] {};
+		// print connected components
+		for (auto it : ump)
+			components[it.second].push_back(it.first);
+		std::cout << count << " components\n";
+		for (size_t i = 0; i < count; ++i) {
+			for (const auto& x : components[i])
+				std::cout << x << " ";
+			std::cout << '\n';
+		}
+		delete[] components;
+	}
+private:
+	void DFS_hp(iterator start, std::vector<bool>& marked) {
+		if (start == end()) throw GraphIterOutOfRange("DFS_hp starting iterator out of range");
+		DFS_hp(start->second, marked);
+	}
+
+	void DFS_hp(size_t s, std::vector<bool>& marked) {
+		marked[s] = true;
+		for (const auto& x : _adj[s])
+			if (!marked[x._dest])
+				DFS_hp(x._dest, marked);
+	}
+
+	void DFS_path(size_t s, std::vector<bool>& marked, std::vector<size_t>& edge_to) {
+		marked[s] = true;
+		for (const auto& x : _adj[s])
+			if (!marked[x._dest]) {
+				edge_to[x._dest] = s; // mark source vertex to destination vertex
+				DFS_path(x._dest, marked, edge_to);
+			}
+	}
+
+	void DFS_cc(iterator start, std::vector<bool>& marked, size_t count, std::unordered_map<T, size_t>& ump) {
+		if (start == end()) throw GraphIterOutOfRange("DFS_cc starting iterator out of range");
+		DFS_cc(start->second, marked, count, ump);
+	}
+
+	void DFS_cc(size_t s, std::vector<bool>& marked, size_t count, std::unordered_map<T, size_t>& ump) {
+		marked[s] = true;
+		ump[_keys[s]] = count;
+		for (const auto& x : _adj[s])
+			if (!marked[x._dest])
+				DFS_cc(x._dest, marked, count, ump);
+	}
+public:
+	// traverse a connected component from a source vertex
+	void DFS(iterator start, void(*visit)(Graph<T>& G, iterator i), std::vector<bool>& marked) {
+		if (start == end()) throw GraphIterOutOfRange("DFS starting iterator out of range");
+		visit(*this, start);
+		// we can also set a counter for the number of vertices connected to start
+		// i.e. the size of this connected component |CC|
+		// ++count;
+		marked[start->second] = true;
+		for (const auto& x : _adj[start->second])
+			if (!marked[x._dest])
+				DFS(find_vertex(_keys[x._dest]), visit, marked);
+	}
+
+	/*void BFS(const T& start, void(*visit)(Graph<T>& G, size_t i), std::vector<bool>& marked) {
+	}*/
 
 private:
-	std::vector<std::vector<Edge>> _adj{};
-	std::map<T, size_t> _map{};	// vertex -> indices
+	// data member default initialization
+	std::vector<std::vector<Edge>> _adj{};	// adjacent list
+	std::unordered_map<T, size_t> _map{};	// vertex -> index
+	std::vector<T> _keys{};					// index -> vertex
 	size_t _E{ 0 };	// number of edges
 
-	// no check for the validity of v, w
-	// at() checks it but with time overhead
-	void add_edge_by_indices(int v, int w, double cost) {
+	// require that from and to must be prior to
+	// end() so that they are dereferenceable
+	bool has_edge_unchecked(const_iterator from, const_iterator to) const {
+		return find_dest(to->second, from->second) != _adj[from->second].end();
+	}
+
+	// no check if the edge already exists
+	void add_edge_by_index_unchecked(size_t v, size_t w, double cost = 0.0) {
 		_adj[v].push_back(Edge(w, cost));
 		_adj[w].push_back(Edge(v, cost));
+		++_E;
 	}
 
 	// no check if v exists
 	void add_vertex_unchecked(const T& v) {
 		_adj.push_back(std::vector<Edge>{});
 		_map[v] = _adj.size() - 1;
+		_keys.emplace_back(v);
+	}
+
+	void remove_edge_v_in_w(size_t v, size_t w) {
+		auto p{ find_dest(v, w) };
+		if (p != _adj[w].end()) {
+			_adj[w].erase(p);	--_E;
+		}
+	}
+
+	// get the iterator in _adj[source] that has an edge dest value
+	// return _adj[source].end() if not found
+	std::vector<Edge>::const_iterator find_dest(size_t dest, size_t source) const {
+		auto it = _adj[source].begin();
+		for (; it != _adj[source].end(); ++it)
+			if (it->_dest == dest) break;
+		return it;
+	}
+
+	std::vector<Edge>::iterator find_dest(size_t dest, size_t source) {
+		auto it = _adj[source].begin();
+		for (; it != _adj[source].end(); ++it)
+			if (it->_dest == dest) break;
+		return it;
+	}
+
+	void modify_weight_unchecked(iterator from, iterator to, double cost) {
+		find_dest(to->second, from->second)->_cost = cost;
+		find_dest(from->second, to->second)->_cost = cost;
 	}
 };
 
--- Graph/Graph_test1.cpp
+++ Graph/Graph_test1.cpp
@@ -3,24 +3,67 @@
 #include <iostream>
 
 using namespace std;
+using namespace myGraph;
+
+template<typename T = std::string>
+void print_vertex(Graph<T>& G, typename Graph<T>::iterator i) {
+	cout << G.vertex(i) << endl;
+}
 
 int main()
 {
-	using namespace myGraph;
 
 	//string filename{ "SymbolGraph.txt" };
 	//string filename{ "SymbolGraph_w.txt" };
 	//string filename{ "tinyG.txt" };
 	//string filename{ "tinyG_w.txt" };
-	try {		
-		bool weighted = true;
+
+	bool weighted = true;
+
+	try {
 		Graph<string> G("SymbolGraph_w.txt", weighted);
+		cout << "Orginal: \n";
 		G.output_graph(cout, weighted);
 
+		cout << "\nG.has_path(G.find_vertex(\"LAS\"), G.find_vertex(\"MCO\"))==" <<
+			G.has_path(G.find_vertex("LAS"), G.find_vertex("MCO"));
+
+		vector<string> path{};
+		G.path(G.find_vertex("LAS"), G.find_vertex("MCO"), path);
+		cout << "\nPaths from LAS to MCO can be:\n";
+		for (const auto& x : path)
+			if (x == "LAS") cout << x;
+			else cout << "-->" << x;
+		cout << "\n";
+
+		G.remove_vertex(G.find_vertex("PHX"));
+		G.remove_vertex(G.find_vertex("HOU"));
+
+		cout << "\nAfter remove \"PHX\" & \"HOU\": \n";
+		G.output_graph(cout, weighted);
+
+		cout << "\nDFS SymbolGraph_w.txt:\n";
+		vector<bool>marked(G.vertex_size(), false);
+		G.DFS(G.find_vertex("LAS"), print_vertex, marked);
+
+		cout << "\ntinyG.txt: \n";
 		G.read_file("tinyG.txt", !weighted);
 		G.output_graph(cout, !weighted);
+
+		cout << "\nConnected components: \n";
+		G.connected_component();
+		
 	}
 	catch (const GraphReadException& e) {
+		cout << e.what() << endl;
+	}	
+	catch (const GraphEdgeException& e) {
+		cout << e.what() << endl;
+	}
+	catch (const GraphInternalException& e) {
+		cout << e.what() << endl;
+	}
+	catch (const GraphIterOutOfRange& e) {
 		cout << e.what() << endl;
 	}
 	catch (const runtime_error& e) {
@@ -29,6 +72,6 @@ int main()
 	catch (...) {
 		cout << "Some unknown error occured" << endl;
 	}
-	
+
 	return 0;
 }
