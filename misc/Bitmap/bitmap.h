b03e03519d0571486d5186d7deae74888c5e1105
Mark Taylor <17marktaylor@gmail.com>
2022-03-13 23:43:30 +0800

    Add bitmap

---
+++ misc/Bitmap/bitmap.h
@@ -0,0 +1,47 @@
+#include <vector>
+#include <cmath>
+
+/*
+ * Mapping an integer to a bit.
+ * The idea is simple: instead of using a `char` (8 bits) to tell a number's
+ * presence, we can use just a bit. Note that the minimal unit of memory we can
+ * manipulate in RAM directly is byte. So we'll use some tricks to add a number,
+ * to tell if a number exists, and to delete a number.
+ *
+ * This can be helpful if we want to tell if a number exists in a HUGE set of
+ * numbers. For example, say we have a set of integers of size 2^32. If we use
+ * a char array to represent their presence, it will cost 4 GiB of RAM.
+ * However, we can get it done just with 4 GiB / 8 = 512 MiB by using a bitmap.
+ */
+class Bitmap {
+    std::vector<unsigned char> _bitmap;
+
+public:
+    Bitmap(size_t capacity) : _bitmap(std::ceil(capacity / 8.0), '\0') {};
+
+    void resize(size_t n) {
+        _bitmap.resize(std::ceil(n / 8.0), '\0');
+    }
+
+    size_t size() const {
+        return _bitmap.size() * 8;
+    }
+
+    void insert(unsigned n) {
+        const size_t index = n / 8;
+        const unsigned char r = n % 8; // set the rth bit one
+        _bitmap[index] = _bitmap[index] | static_cast<unsigned char>(1 << r);
+    }
+
+    bool present(unsigned n) const {
+        const size_t index = n / 8;
+        const unsigned char r = n % 8;
+        return (_bitmap[index] >> r) & 1;
+    }
+
+    void erase(unsigned n) {
+        const size_t index = n / 8;
+        const unsigned char r = n % 8; // set the rth bit zero
+        _bitmap[index] = _bitmap[index] & ~static_cast<unsigned char>(1 << r);
+    }
+};
---
+++ misc/Bitmap/bitmap_test.cc
@@ -0,0 +1,43 @@
+#include "bitmap.h"
+#include <iostream>
+
+using namespace std;
+
+void test1()
+{
+    Bitmap bitmap(10);
+    for (int i = 0; i < 10; ++i)
+        bitmap.insert(i);
+    cout << bitmap.present(9) << '\n';
+    bitmap.erase(9);
+    cout << bitmap.present(9) << '\n';
+}
+
+void test2()
+{
+    // 2^32 integers, but only using 2^32 B / 8 = 512 MiB of RAM.
+    // If we just to store these 2^32 integers, it will consume
+    // 4 * 2^32 = 16 GiB (HUGE!!) of memory. Even if we use a char
+    // array, it will cost 2^32 B = 4 GiB of memory.
+    // One might think of virtual memory, which, theoretically, can
+    // be "infinite" (magnitude of TiB at least). But don't forget
+    // virtual memory and paging is painfully slow.
+    unsigned capacity = static_cast<unsigned>(-1);
+    Bitmap bitmap(capacity);
+    for (size_t i = 0; i < capacity; ++i)
+        bitmap.insert(i);
+
+    bitmap.erase(2017);
+    bitmap.erase(2021);
+    bitmap.erase(2022);
+    bitmap.erase(2024);
+    for (int year = 2017; year < 2027; ++year)
+        cout << year << ':' << bitmap.present(year) << '\n';
+}
+
+int main()
+{
+    test1();
+    test2();
+    return 0;
+}
